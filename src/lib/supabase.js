4f3d866e14ab6116e18cec2123c50d34ff611a7a
Miguel Morales <miguelm0rales017@gmail.com>
2026-02-23 15:06:43 -0500

    fix: hardcodear credenciales Supabase para Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://kobojmrvssqgnidmgddl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvYm9qbXJ2c3NxZ25pZG1nZGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4Njc0MTksImV4cCI6MjA4NzQ0MzQxOX0.pb6uAQeRS_iWmRkyqGb-zDJ18_S1N_H26Q0RhcYNRSo'
 )
